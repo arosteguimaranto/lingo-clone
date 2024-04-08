e7f7201bba4262f77dc81bc2cf731b25b3e4856f
arosteguimaranto <arosteguimaranto@outlook.com>
2024-04-08 17:22:02 -0600

    button library

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
    <div>
-    <p className="text-green-500 font-bold "> Hello Lingo!</p>
+  <Button>Click Me</Button>
    </div> 
   )
 }
