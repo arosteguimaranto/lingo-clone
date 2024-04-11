8d4bdc44f579613ebf67043156bfca3949bdd49e
arosteguimaranto <arosteguimaranto@outlook.com>
2024-04-10 18:51:17 -0600

    Main Layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,14 +1,17 @@
+
+import { Sidebar } from '@/components/sidebar';
 import React from 'react'
 
 type Props ={
     children: React.ReactNode;
 };
 
-const MainLayout = ({children}: Props) => {
+const MainLayout = ({children,}: Props) => {
   return (
     <>
-    <main className='pl-[256px] h-full'>
-        <div className='bg-red-500 h-full'>
+    <Sidebar/>
+    <main className="pl-[256px] h-full">
+        <div className="bg-red-500 h-full">
         {children}
         </div>
     
---
+++ components/sidebar.tsx
@@ -0,0 +1,7 @@
+export const Sidebar = () =>{
+  return (
+    <div className=" flex bg-blue-500 h-full lg:w-[256px] fixed">
+      sidebar
+    </div>
+  )
+}
